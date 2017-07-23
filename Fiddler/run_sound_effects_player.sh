8be58c4fb69bb60b4fa88035458a2fa7305b7f40
macdroid53 <macdroid53@gmail.com>
2017-07-23 15:44:52 -0400

    updated some xml and corrected typo in sounds/Fiddler_sound_sequence.xml

--- Fiddler/run_sound_effects_player.sh
+++ Fiddler/run_sound_effects_player.sh
@@ -6,6 +6,7 @@
 #
 export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0
 #sound_effects_player /home/mac/Shows/Fiddler/Fiddler_project.xml
-sound_effects_player Fiddler_project.xml
+#sound_effects_player Fiddler_project.xml
+sound_effects_player --trace-sequencer-level=1 --trace-file sequencer.txt
 #
 # End of file run_sound_effects_player.sh
