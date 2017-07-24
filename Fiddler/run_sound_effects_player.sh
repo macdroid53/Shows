fea095df23b8518d76478205d0aeeaeb9ae38fb7
macdroid53 <macdroid53@gmail.com>
2017-07-24 19:13:12 -0400

    minor mods

--- Fiddler/run_sound_effects_player.sh
+++ Fiddler/run_sound_effects_player.sh
@@ -7,6 +7,6 @@
 export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0
 #sound_effects_player /home/mac/Shows/Fiddler/Fiddler_project.xml
 #sound_effects_player Fiddler_project.xml
-sound_effects_player --trace-sequencer-level=1 --trace-file sequencer.txt
+sound_effects_player --trace-sequencer-level=1 --trace-file /home/mac/Shows/Fiddler/sequencer.log
 #
 # End of file run_sound_effects_player.sh
